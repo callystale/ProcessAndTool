4aa04a3aa7df5b482cea417ebd387bd9b2ea523e
Callysta Le <callystale@NAAMS.STUSGS.RMIT.EDU.VN>
2025-10-29 14:06:25 +0700

    Added flag swapped

--- W1/src/main/java/sept/BubbleSort.java
+++ W1/src/main/java/sept/BubbleSort.java
@@ -3,14 +3,19 @@ package sept;
 public class BubbleSort {
     public static void bubbleSort(Integer[] arr) {
         int n = arr.length;
+        boolean swapped = true;
+
         for (int i = 0; i < n - 1; i++) {
+            swapped = false;
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    swapped = true;
                 }
             }
+            if (!swapped) { break;}
         }
     }
 }
